879e6211d3a86813b82899f4d983f2e379d69bd5
Raqib Zaman <raqib.zaman@gmail.com>
2025-06-27 13:01:22 -0400

    Got the basic UI setup for adding an entry into excel throught the Window GUI

--- gui.py
+++ gui.py
@@ -25,8 +25,8 @@ class Window:
         self.keep_alive = True
 
         # put window in the center of the screen, QoL
-        win_w = 300
-        win_h = 200
+        win_w = 350
+        win_h = 500
         screen_w = self.main.winfo_screenwidth()
         screen_h = self.main.winfo_screenheight()
         x = (screen_w // 2) - (win_w // 2)
@@ -58,6 +58,33 @@ class Window:
         view_vis_ele_btn = tk.Button(self.r3, text="View Visible Elements", bg="blue", fg="white", command=self.view_vis_ele)
         view_vis_ele_btn.pack(expand=True, fill=tk.BOTH, padx=5, pady=(5,10))
 
+        # row 4 label
+        tk.Label(self.main, text="Add Excel Entry").pack()
+
+        # row 4
+        self.r4 = tk.Frame(self.main, bd=2, relief=tk.SOLID)
+        self.r4.pack(fill=tk.BOTH, expand=True)
+
+        tk.Label(self.r4, text="Enter Question Text").pack(anchor="w", padx="5", pady="5")
+        self.entry_Q = tk.Entry(self.r4, width=30)
+        self.entry_Q.pack(anchor="w", padx="5", pady="5")
+
+        tk.Label(self.r4, text="Enter Type").pack(anchor="w", padx="5", pady="5")
+        self.type_opt = ["radio", "text", "select-one", "button"]
+        self.type_selected = tk.StringVar(value=self.type_opt[0])
+        self.type_dropdown = tk.OptionMenu(self.r4, self.type_selected, *self.type_opt)
+        self.type_dropdown.pack(anchor="w", padx="5", pady="5")
+
+        tk.Label(self.r4, text="Enter Value").pack()
+        self.val_txtbox = tk.Text(self.r4, height=10, width=40)
+        self.val_txtbox.pack()
+
+        tk.Button(self.r4, text="Add Excel Entry", bg="gold", fg="black", command=self.add_excel).pack(expand=True, fill=tk.BOTH,padx=5, pady=5)
+
+
+        # need 3 field to enter information (Question, Type, Value)
+        # And submit button.
+
         # test excel files
         print(self.excel_QTV.head())
         print(self.excel_PI.head())
@@ -76,6 +103,17 @@ class Window:
         self.excel_QTV = pd.read_excel("Q_T_V.xlsx")
         self.excel_PI = pd.read_excel("excel_files/PI_QTV.xlsx")
         self.combo_QTV = pd.concat([self.excel_PI, self.excel_QTV], ignore_index=True)
+        # exceldf.to_excel("updated.xlsx", index=False)
+
+    def add_excel(self):
+        print("Add Excel Entry & update")
+        question = self.entry_Q.get()
+        print(question)
+        type = self.type_selected.get()
+        print(type)
+        value = self.val_txtbox.get("1.0", tk.END).strip()
+        print(value)
+        
         # exceldf.to_excel("updated.xlsx", index=False)
 
     def view_vis_ele(self):
@@ -98,41 +136,43 @@ class Window:
 
             vis_e_lst = self.automate.page_visible_info()    # reads last window in focus, so must follow .switch_to
             vis_len = len(vis_e_lst)    
-            
             for idx, e in enumerate(vis_e_lst):
-                # combine Personal Info with generic Ques.Ty.Val panda dataframes
-                #combo_QTV = pd.concat([self.excel_PI, self.excel_QTV], ignore_index=True)
-                e_txt = e.text.strip().lower()
-                a_match = self.combo_QTV[self.combo_QTV.iloc[:,0].apply(
-                    lambda quest: str(quest).lower() in e_txt
-                )]
+                try:
+                    # combine Personal Info with generic Ques.Ty.Val panda dataframes
+                    #combo_QTV = pd.concat([self.excel_PI, self.excel_QTV], ignore_index=True)
+                    e_txt = e.text.strip().lower()
+                    a_match = self.combo_QTV[self.combo_QTV.iloc[:,0].apply(
+                        lambda quest: str(quest).lower() in e_txt
+                    )]
 
-                if not a_match.empty:
-                    print("It's a match!")
-                    type = str(a_match.iloc[0,1]).strip().lower()
-                    value = str(a_match.iloc[0,2]).strip()  # Do not lower, want to preserve casing when inserting into textbox. Do lowering at value check in the handlers
+                    if not a_match.empty:
+                        print("It's a match!")
+                        type = str(a_match.iloc[0,1]).strip().lower()
+                        value = str(a_match.iloc[0,2]).strip()  # Do not lower, want to preserve casing when inserting into textbox. Do lowering at value check in the handlers
 
-                    for i in range(9):
-                        if idx + i >= vis_len:
-                            print("out of vis_len range")
-                            break
-                        
-                        ee = vis_e_lst[idx + i] # I matched the label txt, now I am going to the next input element
-                        match ee.tag_name:
-                            case "input":
-                                if self.automate.input_handling(ee, type, value):
-                                    break
-                            case "select":
-                                if self.automate.select_handling(ee, type, value):
-                                    break
-                            case "textarea":
-                                if self.automate.textarea_handling(ee, type, value):
-                                    break
-                            case "button":
-                                if self.automate.button_handling(ee, type, value):
-                                    break
-                            case _:
-                                pass
+                        for i in range(9):
+                            if idx + i >= vis_len:
+                                print("out of vis_len range")
+                                break
+                            
+                            ee = vis_e_lst[idx + i] # I matched the label txt, now I am going to the next input element
+                            match ee.tag_name:
+                                case "input":
+                                    if self.automate.input_handling(ee, type, value):
+                                        break
+                                case "select":
+                                    if self.automate.select_handling(ee, type, value):
+                                        break
+                                case "textarea":
+                                    if self.automate.textarea_handling(ee, type, value):
+                                        break
+                                case "button":
+                                    if self.automate.button_handling(ee, type, value):
+                                        break
+                                case _:
+                                    pass
+                except Exception:
+                    print("need to get a job...")
 
 
 
